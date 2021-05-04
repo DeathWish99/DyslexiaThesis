8edb008a8191f6f64d8065280d878b5a61243dc1
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-05-04 14:55:16 +0700

    remove joystick and simple grapth

---
+++ Assets/Scripts/MainMenu/ScorePage/CreateGraph.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class CreateGraph
+{
+
+    public static void ShowGraph(List<float> valueList, RectTransform graphContainer, Sprite circleSprite)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float xSize = 50f;
+
+        GameObject lastCircleObj = null;
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (Mathf.Round(valueList[i]) / yMaximum) * graphHeight;
+            GameObject circleObj = CreateCircle(new Vector2(xPosition, yPosition), graphContainer, circleSprite);
+            if(lastCircleObj != null)
+            {
+                CreateDotConnection(lastCircleObj.GetComponent<RectTransform>().anchoredPosition, circleObj.GetComponent<RectTransform>().anchoredPosition, graphContainer);
+            }
+            lastCircleObj = circleObj;
+        }
+    }
+
+    static GameObject CreateCircle(Vector2 anchoredPosition, RectTransform graphContainer, Sprite circleSprite)
+    {
+        GameObject circle = new GameObject("circle", typeof(Image));
+        circle.transform.SetParent(graphContainer, false);
+        circle.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rect = circle.GetComponent<RectTransform>();
+        rect.anchoredPosition = anchoredPosition;
+        rect.sizeDelta = new Vector2(11, 11);
+        rect.anchorMin = new Vector2(0, 0);
+        rect.anchorMax = new Vector2(0, 0);
+
+        return circle;
+    }
+
+    static void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB, RectTransform graphContainer)
+    {
+        GameObject dotConnection = new GameObject("dotConnection", typeof(Image));
+        dotConnection.transform.SetParent(graphContainer, false);
+        dotConnection.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rect = dotConnection.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rect.sizeDelta = new Vector2(distance, 3f);
+        rect.anchorMin = new Vector2(0, 0);
+        rect.anchorMax = new Vector2(0, 0);
+        rect.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rect.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+    }
+}
---
+++ Assets/Scripts/MainMenu/ScorePage/ServeGraphsPerWord.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+using UnityEngine;
+
+public class ServeGraphsPerWord : MonoBehaviour
+{
+    public Sprite circleSprite;
+    public RectTransform graphContainer;
+    public string targetRecord = "akt"; //temporary.
+    private string path;
+    private string fromJsonString;
+    private JSONNode scoreRecords;
+    private UserScore wordToServe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = Application.dataPath + "/ScoreRecords.json";
+        fromJsonString = File.ReadAllText(path);
+        scoreRecords = JSONNode.Parse(fromJsonString);
+        CreateGraphByWord();
+    }
+    
+    void CreateGraphByWord()
+    {
+        foreach(JSONNode node in scoreRecords)
+        {
+            if(node["objectName"] == targetRecord)
+            {
+                wordToServe = new UserScore(node["objectName"], new List<float>());
+
+                foreach(JSONNode score in node["scores"])
+                {
+                    wordToServe.scores.Add(score.AsFloat);
+                }
+                break;
+            }
+        }
+        CreateGraph.ShowGraph(wordToServe.scores, graphContainer, circleSprite);
+        Debug.Log(wordToServe.objectName + " " + wordToServe.scores[0]);
+    }
+
+}
+
+[System.Serializable]
+public class UserScore
+{
+    public string objectName;
+    public List<float> scores;
+
+    public UserScore(string objectName, List<float> scores)
+    {
+        this.objectName = objectName;
+        this.scores = scores;
+    }
+}
