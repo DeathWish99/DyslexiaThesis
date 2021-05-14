b86fa4289bc197727c7cdc639dbccb202ab56c91
ferysanti <dewikarin123@gmail.com>
2021-05-14 19:23:35 +0700

    animasibutton
    masih harus revisi

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    private Animator an;
+    void Start()
+    {
+        an = GetComponent<Animator>();
+    }
+
+    public void OnClick()
+    {
+        an.SetTrigger("Click");
+    }
+}
