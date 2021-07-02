11fb9c7643df7555875546f37e09893c50f91705
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2021-07-02 10:36:24 +0800

    sqlite setup and initial dbcommands

---
+++ Assets/Scripts/Database/DbCommands.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.IO;
+using System;
+
+public class DbCommands : MonoBehaviour
+{
+    string connectionString;
+    private void Start()
+    {
+        connectionString = "URI=file:" + Application.persistentDataPath + "/Database/score.db";
+        Debug.Log(connectionString);
+        CreateDbAndTable();
+
+        InsertScore("test", "90,123124");
+    }
+    public void CreateDbAndTable()
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE IF NOT EXISTS MsScore(Id INTEGER PRIMARY KEY AUTOINCREMENT, Word VARCHAR(100) NOT NULL, Score DOUBLE NOT NULL, InputDatetime DATETIME NOT NULL);";
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+    }
+
+    public void InsertScore(string word, string score)
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO MsScore (Word, Score, InputDatetime) VALUES ('"+word+"','"+score+"', '"+DateTime.Now+"');";
+                Debug.Log(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+    }
+}
