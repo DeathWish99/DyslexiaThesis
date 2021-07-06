773619b1022b303c875542b651c36887af4b474d
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-07-06 16:29:12 +0700

    play sound and music architecture(incomplete)

---
+++ Assets/Scripts/Audio/SoundControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SoundControl : MonoBehaviour
+{
+    public AudioSource mainMenuMusic;
+    public AudioSource playMusic;
+
+    [System.Serializable]
+    public struct SoundEffect{
+        public string sfxName;
+        public AudioSource sfxClip;
+    };
+
+    public List<SoundEffect> soundEffects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeMusic()
+    {
+
+    }
+
+    public void PlayOneStar()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "bintang_1");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+
+    public void PlayTwoStar()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "bintang_2");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+
+    public void PlayThreeStar()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "bintang_3");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+
+    public void PlayCorrect()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "correct");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+
+    public void PlayWrong()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "wrong");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+
+    public void PlayDoorOpen()
+    {
+        var sfx = soundEffects.Find(x => x.sfxName == "door_open");
+        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+    }
+}
