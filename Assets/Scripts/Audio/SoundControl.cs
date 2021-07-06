4470b84a6dace53f663f99c4530bb553be12c824
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-07-06 16:59:48 +0700

    add game music

--- Assets/Scripts/Audio/SoundControl.cs
+++ Assets/Scripts/Audio/SoundControl.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SoundControl : MonoBehaviour
 {
-    public AudioSource mainMenuMusic;
-    public AudioSource playMusic;
+    public AudioClip mainMenuMusic;
+    public AudioClip playMusic;
 
     [System.Serializable]
     public struct SoundEffect{
@@ -14,10 +14,33 @@ public class SoundControl : MonoBehaviour
     };
 
     public List<SoundEffect> soundEffects;
+
+    [SerializeField]
+    private AudioSource musicSource;
+    [SerializeField]
+    private AudioSource sfxSource;
+
+    static private SoundControl instance;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            // Register as singleton if first
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            // Self-destruct if another instance exists
+            Destroy(this);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        PlayMenuMusic();
     }
 
     // Update is called once per frame
@@ -26,44 +49,82 @@ public class SoundControl : MonoBehaviour
 
     }
 
-    public void ChangeMusic()
+    /// <summary>
+    /// Plays the music designed for the menus
+    /// This method is static so that it can be called from anywhere in the code.
+    /// </summary>
+    static public void PlayMenuMusic()
     {
-
+        if (instance != null)
+        {
+            if (instance.musicSource != null)
+            {
+                instance.musicSource.Stop();
+            }
+            Debug.Log(instance.musicSource);
+            instance.musicSource.clip = instance.mainMenuMusic;
+            instance.musicSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Unavailable MusicPlayer component");
+        }
     }
 
-    public void PlayOneStar()
+    /// <summary>
+    /// Plays the music designed for outside menus
+    /// This method is static so that it can be called from anywhere in the code.
+    /// </summary>
+    static public void PlayGameMusic()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "bintang_1");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        if (instance != null)
+        {
+            if (instance.musicSource != null)
+            {
+                instance.musicSource.Stop();
+            }
+            instance.musicSource.clip = instance.playMusic;
+            instance.musicSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Unavailable MusicPlayer component");
+        }
     }
 
-    public void PlayTwoStar()
+    static public void PlayOneStar()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "bintang_2");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "bintang_1").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
     }
 
-    public void PlayThreeStar()
+    static public void PlayTwoStar()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "bintang_3");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "bintang_2").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
     }
 
-    public void PlayCorrect()
+    static public void PlayThreeStar()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "correct");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "bintang_3").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
     }
 
-    public void PlayWrong()
+    static public void PlayCorrect()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "wrong");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "correct").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
     }
 
-    public void PlayDoorOpen()
+    static public void PlayWrong()
     {
-        var sfx = soundEffects.Find(x => x.sfxName == "door_open");
-        sfx.sfxClip.PlayOneShot(sfx.sfxClip.clip);
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "wrong").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
+    }
+
+    static public void PlayDoorOpen()
+    {
+        var sfx = instance.soundEffects.Find(x => x.sfxName == "door_open").sfxClip;
+        sfx.PlayOneShot(sfx.clip);
     }
 }
